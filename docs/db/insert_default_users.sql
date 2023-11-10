3fe87afeffffdaad7f8d6117b3d7dbec566e47e5
D34dlyK1ss <rags.ftjs@gmail.com>
2023-11-10 17:31:28 +0000

    added 2 queries

---
+++ docs/db/insert_default_users.sql
@@ -0,0 +1,2 @@
+INSERT INTO user(username, email, password) VALUES ('Test', 'test@gmail.com', SHA2('test', 256));
+INSERT INTO user(username, email, password) VALUES ('Test2', 'test2@gmail.com', SHA2('test', 256));
