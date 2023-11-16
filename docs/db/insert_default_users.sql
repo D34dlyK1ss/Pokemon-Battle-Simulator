468d58dfb13b32d027e9049b71667a1c77fc4d3c
D34dlyK1ss <rags.ftjs@gmail.com>
2023-11-16 03:14:51 +0000

    updated db

--- docs/db/insert_default_users.sql
+++ docs/db/insert_default_users.sql
@@ -1,2 +1,2 @@
-INSERT INTO user(username, email, password) VALUES ('Test', 'test@gmail.com', SHA2('test', 256));
-INSERT INTO user(username, email, password) VALUES ('Test2', 'test2@gmail.com', SHA2('test', 256));
+INSERT INTO user(username, email, password) VALUES ('Test', SHA2('test@gmail.com', 256), SHA2('test', 256));
+INSERT INTO user(username, email, password) VALUES ('Test2', SHA2('test2@gmail.com', 256), SHA2('test', 256));
